6563f0b3628d426fcdd64b49680177d1d28bc41f
Abika Chairul Yusri <35727186+Abikayusri@users.noreply.github.com>
2020-02-09 15:16:09 +0700

    Update MyPropertyAnimation

--- MyPropertyAnimation-final/app/src/main/java/binar/academy/carlauncher/animationactivities/BaseAnimationActivity.kt
+++ MyPropertyAnimation-final/app/src/main/java/binar/academy/carlauncher/animationactivities/BaseAnimationActivity.kt
@@ -1,40 +1,40 @@
 package binar.academy.carlauncher.animationactivities
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.util.DisplayMetrics
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import binar.academy.carlauncher.R
 
 
 abstract class BaseAnimationActivity : AppCompatActivity() {
-  protected lateinit var car: View
-  protected lateinit var logo: View
-  protected lateinit var frameLayout: View
-  protected var screenHeight = 0f
+    protected lateinit var car: View
+    protected lateinit var logo: View
+    protected lateinit var frameLayout: View
+    protected var screenHeight = 0f
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    setContentView(R.layout.activity_base_animation)
+        setContentView(R.layout.activity_base_animation)
 
-    car = findViewById(R.id.car)
-    logo = findViewById(R.id.logo)
-    frameLayout = findViewById(R.id.container)
-    frameLayout.setOnClickListener { onStartAnimation() }
-  }
+        car = findViewById(R.id.car)
+        logo = findViewById(R.id.logo)
+        frameLayout = findViewById(R.id.container)
+        frameLayout.setOnClickListener { onStartAnimation() }
+    }
 
-  override fun onResume() {
-    super.onResume()
+    override fun onResume() {
+        super.onResume()
 
-    val displayMetrics = DisplayMetrics()
-    windowManager.defaultDisplay.getMetrics(displayMetrics)
-    screenHeight = displayMetrics.heightPixels.toFloat()
-  }
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        screenHeight = displayMetrics.heightPixels.toFloat()
+    }
 
-  protected abstract fun onStartAnimation()
+    protected abstract fun onStartAnimation()
 
-  companion object {
-    val DEFAULT_ANIMATION_DURATION = 2500L
-  }
+    companion object {
+        val DEFAULT_ANIMATION_DURATION = 2500L
+    }
 }
